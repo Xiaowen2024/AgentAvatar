367eab13ca58fd6b3630315a9041d1e14e79b447
Xiaowen Yuan <71858490+Xiaowen2024@users.noreply.github.com>
2024-07-21 13:32:19 -0700

    Create testConversations.ts

---
+++ backend/convex/tests/testConversations.ts
@@ -0,0 +1,26 @@
+import { startConversationMessage } from "../Agent/conversation";
+import {
+  DatabaseReader,
+  internalAction,
+  internalMutation,
+  mutation,
+  query,
+} from '../_generated/server';
+import { v } from 'convex/values';
+import { GameId } from '../ids';
+// test if start conversation message is working
+
+
+export const testConvo = internalAction({
+  args: {},
+  handler: async (ctx, args) => {
+    const a: any = (await startConversationMessage(
+      ctx,
+      'c:1' as GameId<'conversations'>,
+      'p:1' as GameId<'players'>,
+      'p:2' as GameId<'players'>,
+    )) as any;
+    return await a.readAll();
+  },
+});
+
